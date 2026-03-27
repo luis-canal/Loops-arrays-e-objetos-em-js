8903d1a82de41375ad3e88b2fab97be3636d0979
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-03-27 20:10:35 -0300

    encontra o maior valor da array

---
+++ exercicio02.js
@@ -0,0 +1,11 @@
+const numeros = [10,5,8,20,3];
+maior = numeros[0];
+
+for (let i = 1; i < numeros.length; i++) {
+    if (numeros[i] > maior) {
+        maior = numeros[i];
+    }
+}
+
+console.log(numeros)
+console.log("O maior número da lista é: " + maior)
